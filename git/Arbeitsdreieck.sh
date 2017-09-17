734503f6400c4ca88bd109dd15867c40cee60cce
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-09-17 18:38:06 +0200

    schneller kontributieren

---
+++ git/Arbeitsdreieck.sh
@@ -0,0 +1,14 @@
+#!bash
+
+UPSTREAM=$1
+ME=katrinleinweber
+
+git clone $UPSTREAM --depth=1 --no-single-branch --origin upstream
+REPO=`echo $UPSTREAM | cut -f 5 -d / | cut -f 1 -d .` 
+cd $REPO
+
+# construct URL of my fork & set as origin
+UP_USR=`echo $UPSTREAM | cut -f 4 -d /`
+ORIGIN=`echo $UPSTREAM | sed "s;//;//$ME@;"`
+ORIGIN=`echo $ORIGIN | sed -e "s;$UP_USR;$ME;"`
+git remote add origin $ORIGIN
