aee38e50f7f31d72fb968d594225c39cfa90dec0
Katrin Leinweber <kalei@posteo.de>
2018-04-04 21:49:33 +0200

    Compose branch & message appendix more elegantly

--- git-leaf.sh
+++ git-leaf.sh
@@ -6,6 +6,8 @@ set -eux -o pipefail
 # gelernt von stackoverflow.com/questions/6245570#comment74304422_6245587
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 
-git checkout -b `echo $BRANCH | sed -e "s;$BRANCH;$BRANCH-leaf;"`
-git commit --all --untracked-files -m "WIP-leaf"
+APPEND=WIP-leaf
+
+git checkout -b `echo $BRANCH-$APPEND`
+git commit --all --untracked-files -m "$APPEND"
 git checkout $BRANCH
