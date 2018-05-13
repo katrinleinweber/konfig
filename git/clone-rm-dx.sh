0c7ede08e54e78259e6adcc6cb2a28dcbab3ed72
Katrin Leinweber <kalei@posteo.de>
2018-05-13 13:48:19 +0200

    Update docu for gfork
    squash

--- git/clone-rm-dx.sh
+++ git/clone-rm-dx.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
-# Semi-automatically update dx.doi.org links on Git{Hub|Lab} to https://doi.org.
+# Semi-automatically update dx.doi.org links on GitHub to https://doi.org.
 #
 # Usage:
 # 1. Copy this gist into a local folder of yours (e.g. ~/forks/).
 # 2. Make it executable with `chmod +x ~/path/to/clone-rm-dx.sh`
-# 3. Add your Git{Hub|Lab} username here:
+# 3. Install the gfork Node.js package and its dependencies.
 # 4. Find target repos on https://github.com/search?o=desc&q=dx.doi&s=indexed&type=Code&utf8=%E2%9C%93
 # 5. Insepct the code: can `dx.doi.org` be savely updated? Inspect tests,
 #    regular expression, etc. in particular. Only proceed if you are very sure
 #    that you don't break anything, that you don't feel comfortable fixing.
-# 6. Copy a repo's clone URL from its Git{Hub|Lab} page.
-# 7. Fork the package's repo. On GitLab, wait for that to finish. On GitHub, you
-#    can proceed right away. Curious, isn't it?
-# 8. In a bash terminal, execute this script with `~/path/to/clone-rm-dx.sh
-#      https://git.host.tld/user/repo.git`.
-# 9. Switch back to your browser and start the pull/merge request.
+# 6. Copy the user- & repo name from its GitHub page in the top left. It has the form "username/reponame".
+# 7. In a bash terminal, execute this script with `~/path/to/clone-rm-dx.sh username/reponame`.
+# 8. Fill put the pull request form.
 
 set -eux -o pipefail
 # learned from codeinthehole.com/tips/bash-error-reporting
@@ -25,10 +22,9 @@ REPO=`echo $1 | cut -f 2 -d /`
 gfork --depth=2 $1
 cd $REPO
 
-# prepare pull/merge request on Git{Hub|Lab}
+# prepare pull request on GitHub
 BRANCH=resolve-DOIs-securely
 git checkout -b $BRANCH
-
 
 # learned from https://stackoverflow.com/a/19861378
 grep \
@@ -40,11 +36,12 @@ grep \
   * | xargs -I@ sed -Ei '' \
   's_https?://(dx\.)?doi\.org_https://doi.org_g' @
 
-# prepare pull/merge request on Git{Hub|Lab}
-git add *
-git commit -m "Hyperlink DOIs against preferred resolver"
-git push -u origin $BRANCH
-ME=echo `git remote get-url origin` | cut -f 4 -d /
+# Start pull request
+git commit --all -m "Hyperlink DOIs against preferred resolver"
+git push --set-upstream origin $BRANCH
+ME=echo `git remote get-url origin`
+ME=`echo $ME | cut -f 4 -d /`
 open https://github.com/$1/compare/master...$ME:$BRANCH
 
+# clean up
 rm -rf $REPO
