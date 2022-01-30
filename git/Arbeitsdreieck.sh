6f2404a6292a6a321701acaa2073928ed20a661b
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2022-01-30 11:08:32 +0100

    Deprecate in favor of maintained, versatile tools

--- git/Arbeitsdreieck.sh
+++ git/Arbeitsdreieck.sh
@@ -1,34 +1,5 @@
 #!/usr/bin/env bash
 
-# erste Version & Erklärung auf:
-# gist.github.com/katrinleinweber/f89a07bda89bd9f98b34a831b8c2105a
-
-set -eux -o pipefail
-# gelernt von codeinthehole.com/tips/bash-error-reporting
-
-UPSTREAM="$1"
-ME=katrinleinweber
-BRANCH=""
-
-if [ $# -gt 1 ]; then
-    BRANCH="--branch=$2"
-fi
-
-# konstruiere URL meines fork, mit user@ & clone davon
-UP_USR=$(echo "$UPSTREAM" | cut -f 4 -d /)
-ORIGIN=$(echo "$UPSTREAM" | sed "s;//;//$ME@;")
-ORIGIN=$(echo "$ORIGIN" | sed -e "s;/$UP_USR/;/$ME/;")
-
-git clone \
-	--depth=2 \
-	--shallow-submodules \
-    --no-single-branch \
-	"$BRANCH" \
-	"$ORIGIN"
-
-REPO=$(echo $UPSTREAM | cut -f 5 -d / | sed "s/\.git$//")
-
-st "$REPO"
-cd "$REPO"
-git remote add upstream "$UPSTREAM"
-echo "$REPO" | pbcopy
+echo "Better use https://hub.github.com#contributor"
+echo "or https://github.com/tj/git-extras/blob/master/Commands.md#git-fork"
+echo ";-)"
